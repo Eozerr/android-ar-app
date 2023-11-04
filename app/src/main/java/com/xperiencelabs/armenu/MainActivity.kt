2da89a55f0e495738e3028e06082928363710d18
Ekrem Özer <ekrem3609@gmail.com>
2023-11-04 19:06:36 +0300

    AR screen added

--- app/src/main/java/com/xperiencelabs/armenu/MainActivity.kt
+++ app/src/main/java/com/xperiencelabs/armenu/MainActivity.kt
@@ -40,7 +40,7 @@ class MainActivity : ComponentActivity() {
                       val currentModel = remember {
                           mutableStateOf("sofa")
                       }
-
+                      ARScreen(currentModel.value)
                       Menu(modifier = Modifier.align(Alignment.BottomCenter)){
                           currentModel.value = it
                       }
@@ -91,7 +91,65 @@ fun Menu(modifier: Modifier,onClick:(String)->Unit) {
 }
 
 
+@Composable
+fun ARScreen(model:String) {
+    val nodes = remember {
+        mutableListOf<ArNode>()
+    }
+    val modelNode = remember {
+        mutableStateOf<ArModelNode?>(null)
+    }
+    val placeModelButton = remember {
+        mutableStateOf(false)
+    }
+    Box(modifier = Modifier.fillMaxSize()){
+        ARScene(
+            modifier = Modifier.fillMaxSize(),
+            nodes = nodes,
+            planeRenderer = true,
+            onCreate = {arSceneView ->
+                arSceneView.lightEstimationMode = Config.LightEstimationMode.DISABLED
+                arSceneView.planeRenderer.isShadowReceiver = false
+                modelNode.value = ArModelNode(arSceneView.engine,PlacementMode.INSTANT).apply {
+                    loadModelGlbAsync(
+                        glbFileLocation = "models/${model}.glb",
+                        scaleToUnits = 0.8f
+                    ){
 
+                    }
+                    onAnchorChanged = {
+                        placeModelButton.value = !isAnchored
+                    }
+                    onHitResult = {node, hitResult ->
+                        placeModelButton.value = node.isTracking
+                    }
+
+                }
+                nodes.add(modelNode.value!!)
+            },
+            onSessionCreate = {
+                planeRenderer.isVisible = false
+            }
+        )
+        if(placeModelButton.value){
+            Button(onClick = {
+                modelNode.value?.anchor()
+            }, modifier = Modifier.align(Alignment.Center)) {
+                Text(text = "Place It")
+            }
+        }
+    }
+
+
+    LaunchedEffect(key1 = model){
+        modelNode.value?.loadModelGlbAsync(
+            glbFileLocation = "models/${model}.glb",
+            scaleToUnits = 0.8f
+        )
+        Log.e("errorloading","ERROR LOADING MODEL")
+    }
+
+}
 data class Furniture(var name:String,var imageId:Int)
 
 
